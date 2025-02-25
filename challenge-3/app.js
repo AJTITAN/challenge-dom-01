e22be753884365db49ed7ecc41efd796068f65c5
Ashish <ashish.in.work@gmail.com>
2025-02-26 00:23:22 +0530

    challenge-03-done

--- challenge-3/app.js
+++ challenge-3/app.js
@@ -1,3 +1,24 @@
 /**
  * Write your challenge solution here
  */
+const nameInput = document.getElementById("nameInput");
+const jobInput = document.getElementById("jobInput");
+const ageInput = document.getElementById("ageInput");
+const bioInput = document.getElementById("bioInput");
+const Name = document.getElementById("nameDisplay");
+const Job = document.getElementById("jobDisplay");
+const Age = document.getElementById("ageDisplay");
+const Bio = document.getElementById("bioDisplay");
+
+nameInput.addEventListener("input",()=>{
+Name.innerText = nameInput.value || "Not provided" 
+})
+jobInput.addEventListener("input",()=>{
+Job.innerText = jobInput.value || "Not provided"
+})
+ageInput.addEventListener("input",()=>{
+Age.innerText = ageInput.value || "Not provided"
+})
+bioInput.addEventListener("input",()=>{
+Bio.innerText = bioInput.value || "Not provided"
+})
