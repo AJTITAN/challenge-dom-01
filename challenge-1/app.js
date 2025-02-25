4d7d6ba5d9c430b4b18069f31d62682d6588af17
Ashish <ashish.in.work@gmail.com>
2025-02-25 13:54:40 +0530

    challenge-01-done

--- challenge-1/app.js
+++ challenge-1/app.js
@@ -1,3 +1,27 @@
 /**
  * Write your challenge solution here
  */
+const body = document.getElementById("body");
+const toggleButton = document.getElementById("toggleButton");
+const bulb = document.getElementById("bulb");
+const Status = document.getElementById("status");
+function On() {
+  body.classList.add("dark-mode");
+  bulb.classList.remove("off");
+  toggleButton.innerText = "Turn Off";
+  Status.innerText = "Status: On";
+}
+function Off() {
+  body.classList.remove("dark-mode");
+  bulb.classList.add("off");
+  toggleButton.innerText = "Turn On";
+  Status.innerText = "Status: Off";
+}
+On();
+toggleButton.addEventListener("click", () => {
+  if (body.classList.contains("dark-mode")) {
+    Off();
+  } else {
+    On();
+  }
+});
